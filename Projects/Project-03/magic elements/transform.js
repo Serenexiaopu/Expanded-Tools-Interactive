59cbb40d5e71f62878586dc671b2537903bcc2b5
Serene Xiaopu Deng <xiaopudeng@cca.edu>
2019-04-29 05:34:52 -0700

    up

---
+++ Projects/Project-03/magic elements/transform.js
@@ -0,0 +1,263 @@
+$(document).ready(function(){
+
+$('#one').hover(function(){
+	$('.fire').hide();
+    $('.circle').fadeIn();
+    $('.circle1').fadeIn();
+    $('.circle2').fadeIn();
+    $('.circle3').fadeIn();
+    $('.circle4').fadeIn();
+    $('.circle5').fadeIn();
+    $('.circle6').fadeIn();
+    $('.circle7').fadeIn();
+    $('.circle8').fadeIn();
+    $('.firesymbol').hide();
+    $('.line').hide();
+    $('.drop').hide();
+    $('.drop1').hide();
+    $('.drop2').hide();
+    $('.drop3').hide();
+    $('.drop4').hide();
+    $('.drop5').hide();
+    $('.drop6').hide();
+    $('.flow').hide();
+
+    $('.earth').fadeIn();
+    $('.water').fadeIn();
+    $('.air').fadeIn();
+
+    
+   });
+ 
+  $('#one').click(function(){
+    $('.circle').hide();
+    $('.circle1').hide();
+    $('.circle2').hide();
+    $('.circle3').hide();
+    $('.circle4').hide();
+    $('.circle5').hide();
+    $('.circle6').hide();
+    $('.circle7').hide();
+    $('.circle8').hide();
+    $('.firesymbol').fadeIn();
+
+
+   });
+
+  $('#two').hover(function(){
+	$('.fire').fadeIn();
+	$('.earth').hide();
+    $('.circle').hide();
+    $('.circle1').hide();
+    $('.circle2').hide();
+    $('.circle3').hide();
+    $('.circle4').hide();
+    $('.circle5').hide();
+    $('.circle6').hide();
+    $('.circle7').hide();
+    $('.circle8').hide();
+    $('.flow').hide();
+    $('.firesymbol').hide();
+
+    $('.line').fadeIn();
+    $('.earthsymbol').hide();
+    $('.rec1').hide();
+    $('.drop').hide();
+    $('.drop1').hide();
+    $('.drop2').hide();
+    $('.drop3').hide();
+    $('.drop4').hide();
+    $('.drop5').hide();
+    $('.drop6').hide();
+    $('.water').fadeIn();
+    $('.air').fadeIn();
+
+    
+   });
+ 
+ 
+
+  $('#two').click(function(){
+  	$('.rec1').fadeIn();
+    $('.line').hide();
+    $('.earthsymbol').fadeIn();
+    
+
+
+
+   });
+
+  $('#three').hover(function(){
+  	$('.water').hide();
+
+  	$('.drop').fadeIn();
+  	$('.drop1').fadeIn();
+  	$('.drop2').fadeIn();
+  	$('.drop3').fadeIn();
+  	$('.drop4').fadeIn();
+  	$('.drop5').fadeIn();
+  	$('.drop6').fadeIn();
+	$('.fire').fadeIn();
+	$('.earth').fadeIn();
+	$('.air').fadeIn();
+    $('.circle').hide();
+    $('.circle1').hide();
+    $('.circle2').hide();
+    $('.circle3').hide();
+    $('.circle4').hide();
+    $('.circle5').hide();
+    $('.circle6').hide();
+    $('.circle7').hide();
+    $('.circle8').hide();
+    $('.flow').hide();
+    $('.watersymbol').hide();
+
+    $('.line').hide();
+    $('.earthsymbol').hide();
+    $('.firesymbol').hide();
+    $('.rec1').hide();
+
+
+
+    
+   });
+ 
+ 
+
+ $('#three').click(function(){
+    $('.drop').hide();
+    $('.drop1').hide();
+    $('.drop2').hide();
+    $('.drop3').hide();
+    $('.drop4').hide();
+    $('.drop5').hide();
+    $('.drop6').hide();
+    $('.watersymbol').fadeIn();
+
+    
+
+   });
+
+ $('#four').hover(function(){
+  	$('.air').hide();
+
+  	$('.flow').fadeIn();
+  	$('.water').fadeIn();
+	$('.fire').fadeIn();
+	$('.earth').fadeIn();
+    $('.circle').hide();
+    $('.circle1').hide();
+    $('.circle2').hide();
+    $('.circle3').hide();
+    $('.circle4').hide();
+    $('.circle5').hide();
+    $('.circle6').hide();
+    $('.circle7').hide();
+    $('.circle8').hide();
+    $('.drop').hide();
+    $('.drop1').hide();
+    $('.drop2').hide();
+    $('.drop3').hide();
+    $('.drop4').hide();
+    $('.drop5').hide();
+    $('.drop6').hide();
+    $('.watersymbol').hide();
+
+    $('.line').hide();
+    $('.earthsymbol').hide();
+    $('.firesymbol').hide();
+    $('.airsymbol').hide();
+    $('.rec2').hide();
+
+
+
+    
+   });
+ 
+ 
+
+ $('#four').click(function(){
+    $('.flow').hide();
+   $('.rec2').show();
+    $('.airsymbol').fadeIn();
+
+    
+
+   });
+
+$('.eye').hover(function(){
+  	$('.air').fadeIn();
+  	$('.water').fadeIn();
+	$('.fire').fadeIn();
+	$('.earth').fadeIn();
+
+    $('.circle').hide();
+    $('.circle1').hide();
+    $('.circle2').hide();
+    $('.circle3').hide();
+    $('.circle4').hide();
+    $('.circle5').hide();
+    $('.circle6').hide();
+    $('.circle7').hide();
+    $('.circle8').hide();
+    $('.drop').hide();
+    $('.drop1').hide();
+    $('.drop2').hide();
+    $('.drop3').hide();
+    $('.drop4').hide();
+    $('.drop5').hide();
+    $('.drop6').hide();
+    $('.flow').hide();
+    $('.line').hide();
+
+    $('.watersymbol').hide();
+    $('.earthsymbol').hide();
+    $('.firesymbol').hide();
+    $('.airsymbol').hide();
+    $('.rec2').hide();
+    $('.rec1').hide();
+
+
+
+    
+   });
+
+$('.eye').click(function(){
+  	$('.air').hide();
+  	$('.water').hide();
+	$('.fire').hide();
+	$('.earth').hide();
+
+    $('.circle').hide();
+    $('.circle1').hide();
+    $('.circle2').hide();
+    $('.circle3').hide();
+    $('.circle4').hide();
+    $('.circle5').hide();
+    $('.circle6').hide();
+    $('.circle7').hide();
+    $('.circle8').hide();
+    $('.drop').hide();
+    $('.drop1').hide();
+    $('.drop2').hide();
+    $('.drop3').hide();
+    $('.drop4').hide();
+    $('.drop5').hide();
+    $('.drop6').hide();
+    $('.flow').hide();
+    $('.line').hide();
+
+    $('.watersymbol').fadeIn();
+    $('.earthsymbol').fadeIn();
+    $('.firesymbol').fadeIn();
+    $('.airsymbol').fadeIn();
+    $('.rec2').fadeIn();
+    $('.rec1').fadeIn();
+
+
+
+    
+   });
+
+
+   });
